273d680107fac393fdc5d86b744dabfad5b3abae
Bayo Erinle <berinle@gmail.com>
2012-11-12 06:38:12 -0500

    initial database migration -- build on this

--- grails-app/migrations/changelog.groovy
+++ grails-app/migrations/changelog.groovy
@@ -1,18 +1,8 @@
 databaseChangeLog = {
 
-	changeSet(author: "berinle (generated)", id: "1350257677712-1") {
-		createTable(tableName: "person") {
-			column(autoIncrement: "true", name: "id", type: "bigint") {
-				constraints(nullable: "false", primaryKey: "true", primaryKeyName: "personPK")
-			}
-
-			column(name: "version", type: "bigint") {
-				constraints(nullable: "false")
-			}
-
-			column(name: "first_name", type: "varchar(255)") {
-				constraints(nullable: "false")
-			}
-		}
+	changeSet(author: "berinle (generated)", id: "changelog") {
+		// TODO add changes and preconditions here
 	}
+
+	include file: 'changelog-1.0.groovy'
 }
